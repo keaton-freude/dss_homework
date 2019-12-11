1b631fcaca80c741b9db71cb0a5b371b8210ca51
Keaton Freude <freudek90@gmail.com>
2019-12-10 18:20:11 -0800

    Add nholmann::json to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include "httplib.h"
+#include "json.hpp"
 
 int main() {
     std::cout << "Starting!" << std::endl;
-
-    httplib::Client client("jsonplaceholder.typicode.com");
-
-    auto res = client.Get("/todos/1");
+    httplib::Client client("statsapi.mlb.com");
+    
+    auto res = client.Get("/api/v1/schedule?hydrate=game%28content%28editorial%28recap%29%29%29,decisions&date=2019-10-01&sportId=1");
 
     if (res && res->status == 200) {
         std::cout << "Success!" << std::endl;
+        std::cout << res->body << std::endl;
     } else {
         std::cout << "Failure!" << std::endl;
     }
---
+++ thirdparty/json/README.md
@@ -0,0 +1,5 @@
+### Third Party Info
+Name: nlohmann::json
+Repo: https://github.com/nlohmann/json
+Version: 3.7.3
+License: MIT
