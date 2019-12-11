546f6d559ccf8570f5843af05ae86dbe9bbc8a1f
Keaton Freude <freudek90@gmail.com>
2019-12-10 18:49:28 -0800

    Add GLFW/OpenGL to project, add test window

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include "httplib.h"
 #include "json.hpp"
+#include <GLFW/glfw3.h>
+#include "gl/GL.h"
+
+using json = nlohmann::json;
 
 int main() {
-    std::cout << "Starting!" << std::endl;
-    httplib::Client client("statsapi.mlb.com");
-    
-    auto res = client.Get("/api/v1/schedule?hydrate=game%28content%28editorial%28recap%29%29%29,decisions&date=2019-10-01&sportId=1");
+    GLFWwindow *window;
 
-    if (res && res->status == 200) {
-        std::cout << "Success!" << std::endl;
-        std::cout << res->body << std::endl;
-    } else {
-        std::cout << "Failure!" << std::endl;
+    if (!glfwInit()) {
+        return -1;
     }
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
