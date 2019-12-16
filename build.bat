12f8e3d719b61fec666d8d472cb785abe4bc92ef
Keaton Freude <freudek90@gmail.com>
2019-12-16 00:41:58 -0800

    Hopefully working now

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 echo off
 dir
-cd ./build && cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=DEBUG -DOPENSSL_ROOT_DIR=..\\thirdparty\\openssl .. && cmake --build .
+cd ./build && cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=DEBUG -DOPENSSL_ROOT_DIR=thirdparty\\openssl .. && cmake --build .
