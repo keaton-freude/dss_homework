e4a584936fd8c266e5a2b2eca64e5587e1176a60
Keaton Freude <freudek90@gmail.com>
2019-12-16 00:07:39 -0800

    Add build steps to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+cd ./build
+cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=DEBUG ..
+cmake --build .
