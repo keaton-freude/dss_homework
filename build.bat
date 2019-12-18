d5a211ee784a16e8a442f92c7cf43ce5e2870b1f
Keaton Freude <freudek90@gmail.com>
2019-12-17 18:28:19 -0800

    Fix build script to pick up release builds

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 echo off
 dir
-cd ./build && cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=RELEASE .. && cmake --build . --config Release && cd ./bin && 7z a DSSHomework.zip .\Debug\*
+cd ./build && cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=RELEASE .. && cmake --build . --config Release && cd ./bin && 7z a DSSHomework.zip .\Release\*
